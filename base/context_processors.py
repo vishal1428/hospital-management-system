5233f74b287355e2975a244c089bb0d1f5d6d818
vishal nandi <vishalnandi14@gmail.com>
2025-08-10 17:07:11 +0530

    Fix server error: make services optional and context processor robust

--- base/context_processors.py
+++ base/context_processors.py
@@ -4,5 +4,8 @@ def services_processor(request):
     """
     Context processor to make services available globally in all templates
     """
-    services = base_models.Service.objects.all()
+    try:
+        services = base_models.Service.objects.all()
+    except:
+        services = []
     return {'services': services} 
